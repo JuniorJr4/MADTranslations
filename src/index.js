1e0dc2764a9b0a79b83fa7b20677ae79b36c81b4
Clint G <cgardner4@gmail.com>
2023-06-05 18:44:35 +0200

    add drop down menu functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-import './style.css';
+import "../src/style.css";
+
+function dropdownMenu() {
+  const workDropdown = document.querySelector(".work-dropdown");
+  const dropdownMenu = document.querySelector(".dropdown-menu");
+  let timeoutId;
+  //const dropdownItems = document.querySelectorAll(".dropdown-item"); //Needed?
+  function dropdownMenuMouseover() {
+    clearTimeout(timeoutId);
+    dropdownMenu.classList.remove("hidden");
+  }
+  function dropdownMenuTimeout() {
+    timeoutId = setTimeout(() => {
+      dropdownMenu.classList.add("hidden");
+    }, 2000);
+  }
+  function dropdownMenuClickOut(e) {
+    if (!dropdownMenu.contains(e.target) && e.target !== workDropdown) {
+      dropdownMenu.classList.add("hidden");
+    }
+  }
+  workDropdown.addEventListener("mouseover", dropdownMenuMouseover);
+  dropdownMenu.addEventListener("mouseover", dropdownMenuMouseover);
+
+  workDropdown.addEventListener("mouseout", dropdownMenuTimeout);
+  dropdownMenu.addEventListener("mouseout", dropdownMenuTimeout);
+  document.addEventListener("click", dropdownMenuClickOut);
+  //   workDropdown.addEventListener("mouseout", dropdownMenuClickOut);
+  //   dropdownMenu.addEventListener("mouseout", dropdownMenuClickOut);
+}
+
+export default function pageLoader() {
+  dropdownMenu();
+}
+pageLoader();
