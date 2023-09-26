c23eaacf9bcf97d57e19b80522a72e17315a4041
Clint G <cgardner4@gmail.com>
2023-09-26 18:26:47 +0200

    Add blog page

--- src/index.js
+++ src/index.js
@@ -31,86 +31,115 @@ function dropdownMenu() {
   //   dropdownMenu.addEventListener("mouseout", dropdownMenuClickOut);
 }
 function imageSlider() {
-  const slider = document.querySelector(".slider");
-  const prevButton = document.querySelector(".prev");
-  const nextButton = document.querySelector(".next");
-  const slides = slider.querySelectorAll("li");
-  const dotsContainer = document.querySelector(".slider-dots");
-  let currentIndex = 0;
+  console.log(window.location.pathname);
+  // First ensure user is on portfolio page
+  if (
+    window.location.pathname ===
+    "/home/junior4/repos/MADTranslations/src/portfolio.html"
+  ) {
+    const slider = document.querySelector(".slider");
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
+    const slides = slider.querySelectorAll("li");
+    const dotsContainer = document.querySelector(".slider-dots");
+    let currentIndex = 0;
 
-  function showSlide(index) {
-    // hide all slides
+    function showSlide(index) {
+      // hide all slides
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      // show the slide at the given index
+      slides[index].style.display = "block";
+      updateActiveDot(index);
+    }
+
+    function goToSlide(index) {
+      // update the current index
+      currentIndex = index;
+      // show the slide at the new index
+      showSlide(currentIndex);
+    }
+
+    function goToNextSlide() {
+      console.log("dd");
+      // go to the next slide (loop back to the start if at the end)
+      if (currentIndex === slides.length - 1) {
+        goToSlide(0);
+      } else {
+        goToSlide(currentIndex + 1);
+      }
+      console.log("dd");
+    }
+
+    function goToPrevSlide() {
+      // go to the previous slide (loop back to the end if at the start)
+      if (currentIndex === 0) {
+        goToSlide(slides.length - 1);
+      } else {
+        goToSlide(currentIndex - 1);
+      }
+    }
+
+    function updateActiveDot(index) {
+      // remove the active class from all dots
+      const dots = dotsContainer.querySelectorAll(".slider-dot");
+      for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+      }
+      // add the active class to the dot at the given index
+      dots[index].classList.add("active");
+    }
+
+    // create a dot for each slide
     for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      const dot = document.createElement("span");
+      dot.classList.add("slider-dot");
+      // set the active class on the first dot
+      if (i === 0) {
+        dot.classList.add("active");
+      }
+      // add a click event listener to go to the corresponding slide
+      dot.addEventListener("click", () => {
+        goToSlide(i);
+      });
+      dotsContainer.appendChild(dot);
     }
-    // show the slide at the given index
-    slides[index].style.display = "block";
-    updateActiveDot(index);
-  }
 
-  function goToSlide(index) {
-    // update the current index
-    currentIndex = index;
-    // show the slide at the new index
+    // show the first slide by default
     showSlide(currentIndex);
-  }
 
-  function goToNextSlide() {
-    console.log('dd');
-    // go to the next slide (loop back to the start if at the end)
-    if (currentIndex === slides.length - 1) {
-      goToSlide(0);
-    } else {
-      goToSlide(currentIndex + 1);
-    }
-    console.log('dd');
+    // add click event listeners to the buttons
+    prevButton.addEventListener("click", goToPrevSlide);
+    nextButton.addEventListener("click", goToNextSlide);
+    console.log(currentIndex);
   }
+}
 
-  function goToPrevSlide() {
-    // go to the previous slide (loop back to the end if at the start)
-    if (currentIndex === 0) {
-      goToSlide(slides.length - 1);
-    } else {
-      goToSlide(currentIndex - 1);
-    }
-  }
+function showArticleContent() {
+  // First ensure user is on blog page
+  if (
+    window.location.pathname ===
+    "/home/junior4/repos/MADTranslations/src/blog.html"
+  ) {
+    console.log("y");
+    const articleLinks = document.querySelectorAll(".article-link");
 
-  function updateActiveDot(index) {
-    // remove the active class from all dots
-    const dots = dotsContainer.querySelectorAll(".slider-dot");
-    for (let i = 0; i < dots.length; i++) {
-      dots[i].classList.remove("active");
-    }
-    // add the active class to the dot at the given index
-    dots[index].classList.add("active");
-  }
+    articleLinks.forEach((link) => {
+      const article = link.nextElementSibling;
+      const plusSign = link.previousElementSibling;
+      console.log(article, plusSign);
 
-  // create a dot for each slide
-  for (let i = 0; i < slides.length; i++) {
-    const dot = document.createElement("span");
-    dot.classList.add("slider-dot");
-    // set the active class on the first dot
-    if (i === 0) {
-      dot.classList.add("active");
-    }
-    // add a click event listener to go to the corresponding slide
-    dot.addEventListener("click", () => {
-      goToSlide(i);
+      plusSign.addEventListener("click", (e) => {
+        article.classList.toggle("article-expanded");
+      });
     });
-    dotsContainer.appendChild(dot);
   }
-
-  // show the first slide by default
-  showSlide(currentIndex);
-
-  // add click event listeners to the buttons
-  prevButton.addEventListener("click", goToPrevSlide);
-  nextButton.addEventListener("click", goToNextSlide);
-  console.log(currentIndex);
 }
 
 export default function pageLoader() {
   dropdownMenu();
   imageSlider();
+  showArticleContent();
 }
 pageLoader();
